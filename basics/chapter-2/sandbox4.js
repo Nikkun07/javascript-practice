a23c0663468b2204ed3705dee1cdc33121213611
Nikkun <gabriel.nikkun@gmail.com>
2024-04-10 01:37:11 +0800

    Chapter 2 - Boolean Operators

---
+++ basics/chapter-2/sandbox4.js
@@ -0,0 +1,28 @@
+//Booleans & Comparisons;
+console.log(true, false, "true", "false");
+
+//Methods can return boolean values
+let email = 'nikkun.illust@gmail.com';
+let names = ['Gabriel', 'Nikolai'];
+
+
+// let result = email.includes('!');
+// let result = names.includes('Nikolai');
+// console.log(result);
+
+//Comparison Operators
+/* let age = 24;
+console.log(age == 24); //Abstract Equality
+console.log(age == 30);
+console.log(age != 30); //Negation
+console.log(age > 30); 
+console.log(age < 30); 
+console.log(age <= 24); 
+console.log(age >= 24);  */
+
+let name = 'Nikkun';
+console.log(name == 'Nikkun');
+console.log(name == 'nikkun');
+
+console.log(name > 'Gab'); //Alphabetical Order checking
+console.log(name > 'nikkun');
