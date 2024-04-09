9f7ed9c83e5e58544d6663229b487546e6c4d8ba
Nikkun <gabriel.nikkun@gmail.com>
2024-04-10 01:19:08 +0800

    Chapter 2 - Arrays

---
+++ basics/chapter-2/sandbox3.js
@@ -0,0 +1,26 @@
+//Arrays
+
+let ninjas = ['shaun', 'ryu', 'chun-li'];
+
+ninjas[1] = 'ken'; //Overriding String Value inside the array
+console.log(ninjas[1]);
+
+// let ages = [20, 25, 30, 35];
+// console.log(ages);
+
+// let random = ['shaun', 'crystal', 30, 20];
+// console.log(random);
+
+// console.log(ninjas.length); //Length == How many elements are inside the array
+
+//Array Methods
+// let result = ninjas.join('-');
+// console.log(result);
+
+// let result = ninjas.indexOf('chun-li');
+
+// let result = ninjas.concat(['ken', 'crystal']); //Concatenation of new values inside the array
+
+let result = ninjas.push('ken'); //Destructive Method (Changes the original value of a variable)
+result = ninjas.pop(); //Removes a value off the array
+console.log(result);
