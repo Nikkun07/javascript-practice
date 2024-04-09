4690d33acca5e1984f40ba8a826a06a58e28e9a9
Nikkun <gabriel.nikkun@gmail.com>
2024-04-10 01:24:54 +0800

    Chapter 2 - Null and Undefined

--- basics/chapter-2/sandbox3.js
+++ basics/chapter-2/sandbox3.js
@@ -1,9 +1,9 @@
 //Arrays
 
-let ninjas = ['shaun', 'ryu', 'chun-li'];
+// let ninjas = ['shaun', 'ryu', 'chun-li'];
 
-ninjas[1] = 'ken'; //Overriding String Value inside the array
-console.log(ninjas[1]);
+// ninjas[1] = 'ken'; //Overriding String Value inside the array
+// console.log(ninjas[1]);
 
 // let ages = [20, 25, 30, 35];
 // console.log(ages);
@@ -21,6 +21,13 @@ console.log(ninjas[1]);
 
 // let result = ninjas.concat(['ken', 'crystal']); //Concatenation of new values inside the array
 
-let result = ninjas.push('ken'); //Destructive Method (Changes the original value of a variable)
-result = ninjas.pop(); //Removes a value off the array
-console.log(result);
+// let result = ninjas.push('ken'); //Destructive Method (Changes the original value of a variable)
+// result = ninjas.pop(); //Removes a value off the array
+// console.log(result);
+
+//NULL & UNDIFINED
+// Null = Intentional lack of value
+// Undefined = Unintentional lack of value
+
+let age = null;
+console.log(age, age + 3, `then age is ${age}`);
