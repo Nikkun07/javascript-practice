4fff6abba64658f533f27aa01cdfd550432f4747
Nikkun <gabriel.nikkun@gmail.com>
2024-04-11 06:24:33 +0800

    Chapter 3 - Loops
    >For Loops
    >While Loops
    >Do While Loops

---
+++ basics/chapter-3/sandbox.js
@@ -0,0 +1,44 @@
+//for loops
+
+/* for(let i = 0; i < 5; i++)
+{
+    console.log('I will krill myself ' + i);
+}
+
+console.log('Loop Finished'); */
+
+/* const names = ['shaun', 'mario', 'luigi'];
+
+//Dymanic Loop
+for(let i = 0; i < names.length; i++)
+{
+    // console.log(names[i]);
+    let html = `<div>${names[i]}</div>`; 
+    console.log(html);
+} */
+
+//While Loops
+// const names = ['nikkun', 'chi', 'wow']
+// let i = 0;
+
+/* while(i < 5)
+{
+    console.log('in loop: ', i);
+    i++;
+} */
+
+// while (i < names.length)
+// {
+//     console.log(names[i]);
+//     i++;
+// }
+
+//do while Loops
+let i = 5;
+do
+{
+    console.log('val of i is: ', i);
+    i++;
+}
+while(i<5)
+//performs a statement at least once even if it doesnt meet the coindition
