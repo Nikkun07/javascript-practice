37172f2be5ee701a028a0823673bca71e9292547
Nikkun <gabriel.nikkun@gmail.com>
2024-04-13 23:22:03 +0800

    Logical Operators
    >Logical Operators (||, &&)
    >Logical Not Operators

---
+++ basics/chapter-3/sandbox3.js
@@ -0,0 +1,16 @@
+//Logical Operators - OR || and AND &&
+
+const password = 'bruh';
+
+if(password.length >= 12 && password.includes('!'))
+{
+    console.log("That password goes hard");
+}
+else if(password.length >= 8 || password.includes('!') && password.length > 5)
+{
+    console.log("That password is long enough");
+}
+else
+{
+    console.log("You need to have a stronger password.");
+}
