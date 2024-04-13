615b8d08c9547dbc6851265a812535db6ba87c74
Nikkun <gabriel.nikkun@gmail.com>
2024-04-13 23:16:21 +0800

    If Statements
    >If Statements
    >Else & Else If Statements

---
+++ basics/chapter-3/sandbox2.js
@@ -0,0 +1,33 @@
+//If Statements
+
+// const age = 25;
+
+// if (age > 20)
+// {
+//     console.log('You are older than 20 years old');
+// }
+
+// const retards = ['Nikkun', 'Rentikoy', 'Sakuya', 'Nanaomi'];
+
+// if (retards.length > 3)
+// {
+//     console.log("that's a lot of retards");
+// }
+
+
+//else if statements
+// const password = 'thisIsStupid';
+const password = 'thisIsStupid';
+
+if(password.length >= 12)
+{
+    console.log("That password goes hard");
+}
+else if(password.length >= 8)
+{
+    console.log("That password is long enough");
+}
+else
+{
+    console.log("You need to have a stronger password.");
+}
