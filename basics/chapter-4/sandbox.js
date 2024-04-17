7872bc1d0bbcb757fe611323adc76ff6ba87d02a
Nikkun <gabriel.nikkun@gmail.com>
2024-04-17 23:08:13 +0800

    Chapter 4 - Function Declarations & Expressions

---
+++ basics/chapter-4/sandbox.js
@@ -0,0 +1,25 @@
+//Function Decleration
+
+// function greet()
+// {
+//     console.log('Sup Retar!');
+// }
+
+//Function Expression (not available for hoisting)
+const speak = function()
+{
+    console.log("I'm eepy");
+}; //Always end a function expression with a semicolon (codeblock specific)
+
+
+
+
+greet(); //invoking the function
+
+// speak();
+
+//Hoisting 
+function greet()
+{
+    console.log('Sup Retar!');
+}
