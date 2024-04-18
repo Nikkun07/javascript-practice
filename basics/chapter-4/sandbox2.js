f720d4843e9ad54197015fa5e0cc757b9c0e2a46
Nikkun <gabriel.nikkun@gmail.com>
2024-04-18 23:45:55 +0800

    Chapter 4 - Arguments & Parameters

---
+++ basics/chapter-4/sandbox2.js
@@ -0,0 +1,11 @@
+//Arguments & Parameters
+
+const speak = function(name = 'Retard', time = 'Night') //Order of Parameter must be same as order of Argument
+{
+    console.log(`Good ${time}, ${name}`);
+};
+
+console.log(name); //Nothings going to happen because the name variable is locally declared inside the function
+
+speak(/* 'Retard', 'Morning' */); //Order of Argument must be the same order as Parameter
+speak('Guian', 'Day'); 
