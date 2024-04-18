373dcd243d587877659fffdd79c0f32791a90096
Nikkun <gabriel.nikkun@gmail.com>
2024-04-19 01:23:11 +0800

    Chapter 4 - Functions vs Methods

---
+++ basics/chapter-4/sandbox4.js
@@ -0,0 +1,17 @@
+const name = 'Nikkun';
+
+//Functions 
+
+const greet = () => 'Hello';
+
+let result1 = greet ();
+console.log(result1);
+
+//Methods
+
+let result2 = name.toUpperCase();
+console.log(result2);
+
+//Difference of Function from Methods is the way of how you invoke them.
+//Methods are functions. They're associated with an object or a data type.
+//Functions are defined on their own.
