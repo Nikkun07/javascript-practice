bfe2796f343cea1a6aa34a0b979b74e53bc8d204
Nikkun <gabriel.nikkun@gmail.com>
2024-04-22 15:45:48 +0800

    Chapter 5 - Math Objects
    >Different Math keyword as objects and built in functions
    >Random Numbers

---
+++ basics/chapter-5/sandbox4.js
@@ -0,0 +1,23 @@
+//Math Objects
+
+console.log(Math);
+console.log(Math.PI);
+console.log(Math.E);
+
+
+const area = 7.7;
+const area2 = 69.7;
+const area3 = 69.7;
+console.log(Math.round(area));
+
+console.log(Math.floor(area2));
+
+console.log(Math.ceil(area2));
+
+console.log(Math.trunc(area3));
+
+//random numbers
+const random = Math.random();
+
+console.log(random);
+console.log(Math.round(random * 10000));
