9c8ef6d5bac73e83f52da65b78903cb514e1ad86
Nikkun <gabriel.nikkun@gmail.com>
2024-05-01 00:11:57 +0800

    Chapter 7 - Regular Expressions (RegEx)
    >Basic RegEx
    >Simple input Validation

--- basics/chapter-7/sandbox.js
+++ basics/chapter-7/sandbox.js
@@ -9,3 +9,22 @@ form.addEventListener('submit', e =>
     //console.log(username.value);
     console.log(form.username.value); //ID or Name works 
 });
+
+const username = 'nikkun';
+const pattern = /^[a-z]{6,}$/; //RegEx format
+
+// let result = pattern.test(username);
+
+// console.log(result);
+
+// if(result)
+// {
+//     console.log("Regex Test Passed.")
+// }
+// else
+// {
+//     console.log("Regex Test Failed");
+// }
+
+let result = username.search(pattern); //returns an integer. Returns the index position of the match
+console.log(result);
