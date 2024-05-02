95ae969233d0619ecea1d457351efada982ab215
Nikkun <gabriel.nikkun@gmail.com>
2024-05-03 00:32:35 +0800

    Chapter 7 - Keyboard Events

--- basics/chapter-7/sandbox2.js
+++ basics/chapter-7/sandbox2.js
@@ -1,12 +1,13 @@
 const form = document.querySelector('.signup-form');
 const feedBack = document.querySelector('.feedback');
+const usernamePattern = /^[a-zA-Z]{6,12}$/;
 form.addEventListener('submit', e =>
 {
     e.preventDefault();
 
     //Validation
     const username = form.username.value;
-    const usernamePattern = /^[a-zA-Z]{6,12}$/;
+    
 
     if(usernamePattern.test(username))
     {
@@ -21,5 +22,23 @@ form.addEventListener('submit', e =>
         feedBack.textContent = 'That Username is invalid. Input must only contain letters ranging from 6-12 characters long';
         feedBack.classList.remove('valid');
         feedBack.classList.add('invalid');
+    }
+});
+
+//live feedback
+
+form.username.addEventListener('keyup', e =>
+{
+    console.log(e);
+    //console.log(e.target.value, form.username.value);
+    if(usernamePattern.test(e.target.value))
+    {
+        console.log('Passed');
+        form.username.setAttribute('class', 'borderValid');
+    }
+    else
+    {
+        console.log('Failed');
+        form.username.setAttribute('class', 'borderInvalid');
     }
 });
