6e4f0209a3e94b19df75f22fdc45d84525d946b8
Nikkun <gabriel.nikkun@gmail.com>
2024-05-15 20:51:54 +0800

    Chapter 9 - Filter Method

--- basics/chapter-9/sandbox1.js
+++ basics/chapter-9/sandbox1.js
@@ -1,1 +1,25 @@
 const scores = [10, 30, 15, 25, 50, 40, 5];
+
+//filter method
+//Array iteration - check each item (inside callback function) - if check passes, keep item in filtered array.
+
+// const filteredScores = scores.filter((score) =>
+// {
+//     return score > 20;
+// });
+
+// console.log(filteredScores);
+
+const users = 
+[
+    {name: "Nikkun", premium: true},
+    {name: "Renti", premium: false},
+    {name: "Sakuya", premium: true},
+    {name: "Sovo", premium: false},
+    {name: "Chigga", premium: true},
+];
+
+const premUsers = users.filter(user =>user.premium);
+
+
+console.log(premUsers);
