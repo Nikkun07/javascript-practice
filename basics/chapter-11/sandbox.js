b6e0a990f772f127ff76c312cc78a78b15ee0444
Nikkun <gabriel.nikkun@gmail.com>
2024-06-03 22:30:39 +0800

    Chapter 11 - Date and Time methods
    >Introduction to Date/Time
    >Using methods to show Date/Time
    >Using methods to get string of Date/Time
    >Using methods to get Local Time

--- basics/chapter-11/sandbox.js
+++ basics/chapter-11/sandbox.js
@@ -2,3 +2,26 @@
 //Date constructor is used to create date and time
 
 const now = new Date();
+
+console.log(now);
+console.log(typeof now);
+
+//Year, months, days, times.
+console.log('getFullYear: ', now.getFullYear());
+console.log('getMonth: ', now.getMonth());
+console.log('getDate: ', now.getDate());
+console.log('getDay: ', now.getDay());
+console.log('getHours: ', now.getHours());
+console.log('getMinutes: ', now.getMinutes());
+console.log('getSeconds: ', now.getSeconds());
+
+
+
+//Timestamps (can be used to compare two dates together)
+
+console.log('timestamp: ', now.getTime());
+
+//Date Strings
+console.log('Date Now (String): ', now.toDateString());
+console.log('Time Now: ', now.toTimeString());
+console.log('Local Time Now: ', now.toLocaleString());
