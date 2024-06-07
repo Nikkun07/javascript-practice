a712975bdf0962ce8e05c76cfddee735b620e1ee
Nikkun <gabriel.nikkun@gmail.com>
2024-06-07 21:26:11 +0800

    Chapter 12 - Request Error Handling

--- basics/chapter-12/sandbox2.js
+++ basics/chapter-12/sandbox2.js
@@ -9,11 +9,16 @@ request.addEventListener('readystatechange', () =>
 {
     //console.log(request, request.readyState);
 
-    if(request.readyState === 4)
+    if(request.readyState === 4 && request.status === 200)
     {
-        console.log(request.responseText);
+        console.log(request, request.responseText);
+    }
+    else if(request.readyState === 4)
+    {
+        console.log('Status', request.status, ' Error');
+        console.log("Could not fetch the data.");
     }
 });
 
-request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
+request.open('GET', 'https://jsonplaceholder.typicode.com/todoss/');
 request.send();
