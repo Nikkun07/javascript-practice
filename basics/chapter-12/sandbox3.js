7f0918d63fcb62c1a88a1676b8f1a0df23cd777f
Nikkun <gabriel.nikkun@gmail.com>
2024-07-22 23:17:44 +0800

    Chapter 12 - Async & Await
    >Chain promises in a readable way

--- basics/chapter-12/sandbox3.js
+++ basics/chapter-12/sandbox3.js
@@ -1,8 +1,30 @@
 //Fetch API 
 
 //fetch(Endpoint/Local Resource)
+/* Three Steps */
+//Fetch data
+//Take Response, Return Response.json
+//Fire a function when you have access to the data
 
-fetch('nikkun.json').then((response) => 
+
+//asynchronous Function (Always returns a promise)
+const getTodos = async() =>
+{
+    const response = await fetch('nikkun.json');
+    const data = await response.json();
+    return data;
+};
+console.log(1);
+console.log(2);
+console.log(3);
+getTodos()
+    .then(data => console.log('Resolved', data));
+
+console.log(4);
+console.log(5);
+//Non-blocking code. Let's the code continue to run.
+
+/* fetch('nikkun.json').then((response) => 
 {
     console.log('Resolved', response);
     return response.json();
@@ -13,8 +35,5 @@ fetch('nikkun.json').then((response) =>
 ).catch((err) =>
 {
     console.log('rejected', err);
-});
+}); */
 
-//Fetch data
-//Take Response, Return Response.json
-//Fire a function when you have access to the data
