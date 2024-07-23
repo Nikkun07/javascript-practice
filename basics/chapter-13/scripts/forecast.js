96a55b9567a858faa62fe208dc47139a8ba8220a
Nikkun <gabriel.nikkun@gmail.com>
2024-07-23 22:11:29 +0800

    Chapter 13 - Integrated AccuWeather API Functions
    >Get City Info
    >Get Weather Info

--- basics/chapter-13/scripts/forecast.js
+++ basics/chapter-13/scripts/forecast.js
@@ -1,5 +1,8 @@
 const key = 'vC4T1EoPj7gzXphwAkascMO2cbUREzvJ';
 
+
+
+//Get City Info
 const getCity = async (city) => 
 {
     const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
@@ -12,6 +15,24 @@ const getCity = async (city) =>
     return data[0];
 };
 
-getCity('manila')
-    .then(data => console.log(data))
+//Get Weather Information
+const getWeather = async (locID) =>
+{
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${locID}?apikey=${key}`;
+
+    const response = await fetch(base + query);
+
+    const data = await response.json();
+
+    return data[0];
+};
+
+getCity('manila').then(data => 
+    {
+        return getWeather(data.Key);
+    }).then(data => 
+    {
+        console.log(data);
+    })
     .catch(err => console.log(err));
