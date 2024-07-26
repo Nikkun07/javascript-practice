17a6f5f19ccb12eb289e26418dff2467cd5e9c3c
Nikkun <gabriel.nikkun@gmail.com>
2024-07-26 23:33:14 +0800

    Chapter 15 - Class Methods & Method Chaining

--- basics/chapter-15/ObjOrientedProg2.js
+++ basics/chapter-15/ObjOrientedProg2.js
@@ -5,13 +5,37 @@ class User
         //setup properties of the Object
         this.username = username;
         this.email = email;
+        this.score = 0;
+    }
+
+    login()
+    {
+        console.log(`${this.username} just logged in.`);
+        return this;
+    }
+
+    logout()
+    {
+        console.log(`${this.username} just logged out.`);
+        return this;
+    }
+
+    incScore()
+    {
+        this.score++;
+        console.log(`${this.username} has a score of ${this.score}.`);
+        return this;
     }
 }
 
 const userOne = new User('Rappo', 'sleepy.raccoon@email.com');
 const userTwo = new User('Nikkun', 'nikkun.illust@gmail.com');
 
-console.log(userOne, userTwo);
+// console.log(userOne, userTwo);
+// userOne.login();
+// userOne.logout();
+//This wont work unless there is a returned value in the constructed method that is not undefined:
+userOne.login().incScore().incScore().incScore().logout(); 
 
 //the 'new' keyword
 // 1 - it creates a new empty object {}
