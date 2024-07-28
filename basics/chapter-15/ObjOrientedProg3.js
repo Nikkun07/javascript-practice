1f8225b6a61fb45014e0809b1d54c3346fe73d12
Nikkun <gabriel.nikkun@gmail.com>
2024-07-28 21:19:55 +0800

    Chapter 15 - Prototypal Inheritance & Built-in Objects

--- basics/chapter-15/ObjOrientedProg3.js
+++ basics/chapter-15/ObjOrientedProg3.js
@@ -18,6 +18,18 @@ function User(username, email)
     this.email = email;
 }
 
+function Admin(username, email, title)
+{
+    User.call(this, username, email, title);
+    this.title = title;
+}
+
+Admin.prototype = Object.create(User.prototype);
+Admin.prototype.deleteUser = function()
+{
+    //delete a user
+}
+
 User.prototype.login = function()
 {
     console.log(`${this.username} has logged in.`);
@@ -31,10 +43,12 @@ User.prototype.logout = function()
     return this;
 }
 
+
 const userOne = new User('Rappo', 'sleepy.raccoon@email.com');
 const userTwo = new User('Nikkun', 'nikkun.illust@gmail.com');
+const userThree = new Admin('Claire', 'Claire.Test@gmail.com', 'Head Admin');
 
 
-console.log(userOne, userTwo);
+console.log(userOne, userTwo, userThree);
 userOne.login();
 userOne.logout();
