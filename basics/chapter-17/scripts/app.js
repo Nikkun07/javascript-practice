31ac108d4b251ebca7427f619297c95865627839
Nikkun <gabriel.nikkun@gmail.com>
2024-08-06 11:07:46 +0800

    Chapter 17 - Capture and Send Message Function

--- basics/chapter-17/scripts/app.js
+++ basics/chapter-17/scripts/app.js
@@ -1,14 +1,22 @@
 //DOM Queries
 const chatList = document.querySelector('.chat-list');
+const newChatForm = document.querySelector('.new-chat');
 
 //Class Instances
 const chatUI = new ChatUI(chatList);
 const chatroom = new Chatroom('general', 'Sakuya');
 
 
+//Add New Chat
+newChatForm.addEventListener('submit', e =>
+    {
+        e.preventDefault();
+        const msg = newChatForm.message.value.trim();
+        chatroom.addChat(msg)
+            .then(() => newChatForm.reset())
+            .catch(err => console.log(err));
+    });
+
+    
 //Get chats and render 
-chatroom.getChatMessages((data) =>
-{
-    console.log(data);
-    chatUI.render(data);
-});
+chatroom.getChatMessages(data =>chatUI.render(data));
