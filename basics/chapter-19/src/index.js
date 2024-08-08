236124fadac9015628e49c83a6d9643a92d83885
Nikkun <gabriel.nikkun@gmail.com>
2024-08-08 15:22:08 +0800

    Chapter 19 - NPM Scripts & Watching Files

---
+++ basics/chapter-19/src/index.js
@@ -0,0 +1,16 @@
+const greet = (name) =>
+{
+    console.log(`Hello ${name}`);
+}
+
+greet('Nikkun');
+greet('Sakuya');
+greet('Renti');
+
+class User
+{
+    constructor()
+    {
+        this.score = 0;
+    }
+}
