9c4b2a9b3dc10bbbfc7e8d72da2df822e88f9567
Nikkun <gabriel.nikkun@gmail.com>
2024-08-09 18:13:16 +0800

    Chapter 19 - Incorporating Babel + Webpack

--- basics/chapter-19/webpack.config.js
+++ basics/chapter-19/webpack.config.js
@@ -16,4 +16,20 @@ module.exports =
             publicPath: "/assets/",
         },
     },
+    module:
+    {
+        rules: 
+        [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use:
+            {
+                loader: 'babel-loader',
+                options:
+                {
+                    presets: ['@babel/preset-env']
+                }
+            }
+        }]
+    },
 };
