075b3be5f9cfef300291726a7d5c8025a1fb14f0
Nikkun <gabriel.nikkun@gmail.com>
2024-08-12 12:15:02 +0800

    Chapter 20 - UI Project [Dropdowns]

--- basics/chapter-20/src/index.js
+++ basics/chapter-20/src/index.js
@@ -1,6 +1,17 @@
 import Tooltip from "./ui-files/tooltip";
+import Dropdown from "./ui-files/dropdown";
 
 // Create Tooltip
 const tooltip = new Tooltip(document.querySelector('.tooltip'));
+
+
+// Create Dropdown
+const dropdowns = document.querySelectorAll('.dropdown');
+
+dropdowns.forEach(dropdown =>
+{
+    const instance = new Dropdown(dropdown);
+    instance.init();
+})
 
 tooltip.init();
